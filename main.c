7019627108e0687d309d25bc54fc21461991945e
Ak1 <github@maurice-johnson.de>
2023-12-04 01:25:55 +0100

    added print

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int main(int argc, char const *argv[])
   printf("\nNow removing element from position 10: \n");
   removeFromDynArr(&pDynArr, 10);
 
+  printf("Elements after removal: \n");
   for (size_t i = 0; i < pDynArr.size; i++)
   {
     printf("%d ", pDynArr.arr[i]);
